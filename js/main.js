e91e2da340e0d033ded6c8444ad533f42238fe65
Jon Rutter <69657008+jonrutter@users.noreply.github.com>
2021-01-17 14:01:09 -0500

    Finished Drafting JS

--- js/main.js
+++ js/main.js
@@ -1,37 +1,13 @@
 const groceryList = document.querySelector(".list");
 const listForm = document.querySelector("#list-form");
 const listInput = document.querySelector("#list-input");
-const listClear = document.querySelector("#list-clear");
-const defaultItems = {name: "Add items below...", checked: false};
-const items = JSON.parse(localStorage.getItem("items")) || [];
-console.log(items);
+const removeCheckedBtn = document.querySelector("#clear-checked");
+const clearListBtn = document.querySelector("#clear-list");
+let items = JSON.parse(localStorage.getItem("items")) || [];
 
-// Need functions to: 
-// Check / uncheck single item
-// Remove checked items
-
-function addItem(e) {
-  e.preventDefault();
-  const name = this.querySelector('input[type="text"]').value;
-  const item = {
-    name,
-    checked: false
-  }
-  items.push(item);
-  localStorage.setItem("items", JSON.stringify(items));
-  updateDisplay(groceryList, items);
-  this.reset();
-}
-
-function toggleCheck(e) {
-  if (!e.target.matches("input")) return;
-  const index = e.target.dataset.index;
-  items[index].checked = !items[index].checked
-
-}
-
+// Renders the list in HTML
 function updateDisplay(list, items = []) {
-  groceryList.innerHTML = items.reduce((acc, item, index) => {
+  list.innerHTML = items.reduce((acc, item, index) => {
     return acc + `
       <li class="list-item">
         <input id="item${index}" type="checkbox" data-index="${index}" ${item.checked ? "checked" : "" }>
@@ -41,14 +17,40 @@ function updateDisplay(list, items = []) {
   }, ``);
 }
 
+// Form handler: adds the input value to the list of items
+function addItem(e) {
+  e.preventDefault(); // Preventing page refresh on submit
+  const name = this.querySelector('input[type="text"]').value;
+  const item = { name, checked: false }
+  items.push(item);
+  localStorage.setItem("items", JSON.stringify(items));
+  updateDisplay(groceryList, items);
+  this.reset(); // Clears form input field
+}
+
+// Updates checkbox state on click
+function toggleCheck(e) {
+  if (!e.target.matches("input")) return;
+  const index = e.target.dataset.index;
+  items[index].checked = !items[index].checked;
+  localStorage.setItem("items", JSON.stringify(items));
+}
+
+// Removes all checked items from list
+function removeChecked() {
+  items = items.filter(i => !i.checked);
+  localStorage.setItem("items", JSON.stringify(items))
+  updateDisplay(groceryList, items);
+}
+
+// Clears entire list
 function clearList() {
   items.length = 0;
   localStorage.removeItem("items");
-  updateDisplay(groceryList, [defaultItems]);
+  updateDisplay(groceryList);
 }
 
-if (items.length !== 0) updateDisplay(groceryList, items);
-else updateDisplay(groceryList, [defaultItems]);
 listForm.addEventListener("submit", addItem);
-listClear.addEventListener("click", clearList);
 groceryList.addEventListener("click", toggleCheck);
+clearListBtn.addEventListener("click", clearList);
+removeCheckedBtn.addEventListener("click", removeChecked);
